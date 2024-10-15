3e3b4accbcd56783c995b9f00fa8eb20d70e699e
Виталий Гаджиян <vita1ka@inbox.ru>
2024-10-15 19:05:00 +0400

    style fix

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
         textButtonTheme: TextButtonThemeData(
-          style: ElevatedButton.styleFrom(
+          style: TextButton.styleFrom(
             backgroundColor: Colors.blueGrey,
             foregroundColor: Colors.white,
             shape: RoundedRectangleBorder(
